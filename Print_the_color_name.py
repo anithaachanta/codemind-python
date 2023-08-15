834371f81702df8fc4d313ed741979e975cd2d69
anithaachanta <119720845+anithaachanta@users.noreply.github.com>
2023-08-15 21:57:32 +0530

    s=input() if(s=='V'):     print("Violet") elif(s=='I'):     print("Indigo") elif(s=='B'):     print("Blue") elif(s=='G'):     print("Green") elif(s=='Y'):     print("Yellow") elif(s=='O'):     print("Orange") elif(s=='R'):     print("Red") else:     print("-1")

---
+++ Print_the_color_name.py
@@ -0,0 +1,17 @@
+s=input()
+if(s=='V'):
+    print("Violet")
+elif(s=='I'):
+    print("Indigo")
+elif(s=='B'):
+    print("Blue")
+elif(s=='G'):
+    print("Green")
+elif(s=='Y'):
+    print("Yellow")
+elif(s=='O'):
+    print("Orange")
+elif(s=='R'):
+    print("Red")
+else:
+    print("-1")
